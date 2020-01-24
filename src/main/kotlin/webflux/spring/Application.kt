52a1fb563a423f43b8aa520b43d103b0141a16dd
yuta <nagaiyuta@team-lab.com>
2020-01-24 21:31:49 +0900

    init commit

---
+++ src/main/kotlin/webflux/spring/Application.kt
@@ -0,0 +1,13 @@
+package webflux.spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.reactive.config.EnableWebFlux
+
+@SpringBootApplication
+@EnableWebFlux
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
